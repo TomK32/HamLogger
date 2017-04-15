a28b71e29a2e9e10cd13e274008a04ebd16cccea
Thomas R. Koll <tomk32@tomk32.de>
2017-04-15 09:10:17 +0200

    Fix indention and fix a test

--- test/server/web/index.js
+++ test/server/web/index.js
@@ -15,46 +15,39 @@ let server;
 
 lab.beforeEach((done) => {
 
-    const plugins = [Vision, HomePlugin];
-    server = new Hapi.Server();
-    server.connection({ port: Config.get('/port/web') });
-    server.register(plugins, (err) => {
+  const plugins = [Vision, HomePlugin];
+  server = new Hapi.Server();
+  server.connection({ port: Config.get('/port/web') });
+  server.register(plugins, (err) => {
 
-        if (err) {
-            return done(err);
-        }
+    if (err) {
+      return done(err);
+    }
 
-        server.views({
-            engines: { jade: require('jade') },
-            path: './server/web'
-        });
-
-        done();
+    server.views({
+      engines: { jade: require('jade') },
+      path: './server/web'
     });
+
+    done();
+  });
 });
 
 
-lab.experiment('Home Page View', () => {
+lab.experiment('/', () => {
+  lab.beforeEach((done) => {
+    request = {
+      method: 'GET',
+      url: '/'
+    };
+    done();
+  });
 
-    lab.beforeEach((done) => {
 
-        request = {
-            method: 'GET',
-            url: '/'
-        };
-
-        done();
+  lab.test('home page renders properly', (done) => {
+    server.inject(request, (response) => {
+      Code.expect(response.statusCode).to.equal(200);
+      done();
     });
-
-
-    lab.test('home page renders properly', (done) => {
-
-        server.inject(request, (response) => {
-
-            Code.expect(response.result).to.match(/activate the plot device/i);
-            Code.expect(response.statusCode).to.equal(200);
-
-            done();
-        });
-    });
+  });
 });
